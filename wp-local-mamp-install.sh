14823181e11ac033b2dda0fffd694da8ad600603
xavierartot <artotal@gmail.com>
2014-07-16 11:09:39 -0500

    download theme Roots

--- wp-local-mamp-install.sh
+++ wp-local-mamp-install.sh
@@ -11,25 +11,13 @@ wp core download
 
 echo 'create the database'
 mysql -u root -p"root" -e "CREATE DATABASE \`$rep\`;"
-#mysql -u root -p"$MYSQL_ROOT_PASS" -e "CREATE DATABASE \`$rep\`;"
 
 # Standard wp-config.php file
 # Enable WP_DEBUG and WP_DEBUG_LOG
+echo 'create the file wp-config.php'
 wp core config --dbname=$rep --dbuser=root --dbpass=root
 
-#install Wordpress
-echo 'enter the website title, must have one word'
-read title
-echo 'enter the admin user, must have one word'
-read admin_user
-echo 'enter the admin password, must have 4 letters'
-read admin_password
-wp core install --url=http://localhost:8888/$rep/ --title=$title --admin_user=$admin_user --admin_password=$admin_password --admin_email=admin_email@gmail.fr
+cd wp-content/themes
+git clone https://github.com/roots/roots.git
 
-#install the theme roots and activate it
-wp theme install https://github.com/roots/roots/archive/master.zip --activate
-
-#list the themes installed
-wp theme list
-echo 'the theme was activate'
-
+open http://localhost:8888/$rep
